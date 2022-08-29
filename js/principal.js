bbebce33a9d351588e4e70d64d9df6d27b755d1e
cesaraugustodacosta <cesaraug76@hotmail.com>
2022-08-28 22:57:04 -0300

    Eventos no botao

--- js/principal.js
+++ js/principal.js
@@ -1,15 +1,19 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Cida Nutricionista";
 
+var pacientes = document.querySelectorAll(".paciente");
 
-  var paciente = document.querySelector("#primeiro-paciente");
+for(var i = 0; i < pacientes.length; i++){
+
+  var paciente = pacientes[i];
+
   var tdPeso   = paciente.querySelector(".info-peso");
   var peso = tdPeso.textContent;
 
   var tdAltura = paciente.querySelector(".info-altura");
   var altura = tdAltura.textContent;
 
-  var tdImc = document.querySelector(".info-imc");
+  var tdImc = paciente.querySelector(".info-imc");
 
   var pesoEhValido = true;
   var alturaEhvalida = true;
@@ -18,15 +22,22 @@ titulo.textContent = "Cida Nutricionista";
     console.log("peso inválido");
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido!";
+    paciente.classList.add("paciente-invalido");
   }
 
   if(altura <= 0 || altura >= 3.00 ){
     console.log("Altura inválida");
     alturaEhvalida = false;
     tdImc.textContent = "Altura inválida!";
+    paciente.classList.add("paciente-invalido");
   }
-if( alturaEhvalida && pesoEhValido){
-  var IMC = peso / (altura * altura);
-  tdImc.textContent = IMC;
-  console.log(IMC);
+  if( alturaEhvalida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(){
+  console.log("Oi, cliquei no botão!");
+});
