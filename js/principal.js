a70848f1d6d53c2d4c1d932c407a348785450c34
cesaraugustodacosta <cesaraug76@hotmail.com>
2022-08-29 11:26:38 -0300

    Escutando eventos - Prevent default - adicionando clientes

--- js/principal.js
+++ js/principal.js
@@ -38,6 +38,43 @@ for(var i = 0; i < pacientes.length; i++){
 }
 
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(){
-  console.log("Oi, cliquei no botão!");
+botaoAdicionar.addEventListener("click", function(event){
+  event.preventDefault();
+
+  var form = document.querySelector("#form-adiciona");
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd    = document.createElement("td");
+  var pesoTd    = document.createElement("td");
+  var alturaTd  = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd     = document.createElement("td");
+
+  nomeTd.textContent    = nome;
+  pesoTd.textContent    = peso;
+  alturaTd.textContent  = altura;
+  gorduraTd.textContent = gordura;
+  imcTd.textContent     = imc;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+
+  tabela.appendChild(pacienteTr);
+
+
+
+  console.log(pacienteTr);
+  console.log(form.peso.value);
+  console.log(form.altura.value);
+  console.log(gordura);
 });
